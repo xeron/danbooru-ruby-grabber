e32b0cbbde01b95d2b9bca7bdd5c0d46e97d05f8
Ivan Larionov <xeron.oskom@gmail.com>
2015-05-17 23:45:00 -0700

    Tests cleanup.

--- features/step_definitions/download_images_steps.rb
+++ features/step_definitions/download_images_steps.rb
@@ -12,8 +12,8 @@ Given(/^I want to download images from (.*) and save them using (.*)$/) do |boar
     "-l per_page=#{@per_page} -l pages=#{@pages}",
     "-l posts=#{@posts}"
   ]
-  filenames = pattern == "default pattern" ? "" : " -f #{pattern}"
-  @cmd = "ruby danbooru.rb #{limits.sample}#{filenames} -b #{@board} #{@tag}"
+  filenames = pattern == "default pattern" ? "" : "-f #{pattern}"
+  @cmd = "ruby danbooru.rb #{limits.sample} #{filenames} -b #{@board} #{@tag}"
 end
 
 When(/^I run script to download images using (.*)$/) do |saver|
@@ -26,13 +26,13 @@ When(/^I run script to download images using (.*)$/) do |saver|
 
   output = `#{@cmd}`
 
-  @missed_count = output.split("\n").grep("File url is unknown.").size
-  @files = list_files(@tag)
-  @images = @files - ["files.bbs"]
+  missed_count = output.split("\n").grep("File url is unknown.").size
+  @images_count = @posts - missed_count
 end
 
 Then(/^I should see downloaded images$/) do
-  @images_count = @posts - @missed_count
+  @files = list_files(@tag)
+  @images = @files - ["files.bbs"]
 
   expect(@images.size).to eq @images_count
   @images.each do |image|
@@ -44,7 +44,6 @@ Then(/^I should see images and tags in bbs file$/) do
   bbs = File.open(@bbs_file).read
 
   expect(bbs.split("\n").size).to eq @images_count
-  expect(bbs).to match @tag
   @images.each do |image|
     expect(bbs).to match Regexp.new("^#{Regexp.escape(image)}.*#{@tag}.*\\n")
   end
