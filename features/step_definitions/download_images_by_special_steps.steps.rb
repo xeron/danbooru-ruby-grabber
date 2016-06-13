68b5fd8f8cab71c6b61fd6b9fac82d469eb78f64
Ivan Larionov <xeron.oskom@gmail.com>
2016-06-13 02:11:29 -0700

    Simplify special tags test.

--- features/step_definitions/download_images_by_special_steps.steps.rb
+++ features/step_definitions/download_images_by_special_steps.steps.rb
@@ -1,10 +1,7 @@
 Given(/^I want to download images from (\S+) with special tag and save them using (.*)$/) do |board, pattern|
   @tag = SPECIAL_MATRIX[board]
-  limits = [
-    "-l per_page=#{PER_PAGE} -l pages=#{PAGES}",
-    "-l posts=#{POSTS_COUNT}"
-  ]
+
   filenames = pattern == "default pattern" ? "" : "-f #{pattern}"
 
-  @cmd = "ruby danbooru.rb #{limits.sample} #{filenames} -b #{board} #{@tag}"
+  @cmd = "ruby danbooru.rb -l posts=#{POSTS_COUNT} #{filenames} -b #{board} #{@tag}"
 end
